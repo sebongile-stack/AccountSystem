48e5dd073f26f66c978fc580a622e487c6f5236a
sebongile-stack <88289279+sebongile-stack@users.noreply.github.com>
2021-10-08 19:27:14 +0200

    Improved application
    I improved the application

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/RestServiceApplication.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/RestServiceApplication.java
@@ -4,28 +4,15 @@ package za.ac.nwu.ac.web.sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 
-import java.util.List;
-
-//import oracle.jdbc.OracleDriver;
-
-@SpringBootApplication/*(exclude=DataSourceAutoConfiguration.class)*/
-//@ComponentScan({"za.ac.nwu.ac.translator.AccountTypeTranslator"})
 
 public class RestServiceApplication
 {
-    /*@Autowired
-    private AccountTypeRepository repository;*/
     public static void main(String [] args)
     {
         SpringApplication.run(RestServiceApplication.class, args);
     }
 
-    /*@Override
-    public void run(String... args) throws Exception {
-        String sql = "SELECT * FROM DEMO_ACCOUNT_TYPE";
 
-        List<AccountType> accountType = repository.query(sql, BeanPropertyRowMapper.newInstance(AccountType.class));
-        accountType.forEach(System.out :: println);
-    }*/
 }
